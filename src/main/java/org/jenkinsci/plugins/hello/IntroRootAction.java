149c22e8abd4b98b0773face609cee8377d515c1
Ben Chatelain <ben@octop.ad>
2017-08-29 09:57:03 -0700

    Add IntroRootAction

---
+++ src/main/java/org/jenkinsci/plugins/hello/IntroRootAction.java
@@ -0,0 +1,32 @@
+package org.jenkinsci.plugins.hello;
+
+import hudson.Extension;
+import hudson.model.RootAction;
+
+/**
+ * A custom root action.
+ */
+@Extension
+public class IntroRootAction implements RootAction {
+    /** Returns icon file name.
+     * @return icon file name
+     */
+    @Override
+    public final String getIconFileName() {
+        return "clipboard.png";
+    }
+    /** Returns user visible link text.
+     * @return link display name
+     */
+    @Override
+    public final String getDisplayName() {
+        return "Intro Root Action";
+    }
+    /** Returns link destination URL.
+     * @return link destination URL
+     */
+    @Override
+    public final String getUrlName() {
+        return "https://jenkins.io/";
+    }
+}
